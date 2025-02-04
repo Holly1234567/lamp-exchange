25c152c8a431cbd8355bb06369b60a1f3774ebee
Holly1234567 <bluetrader777@gmail.com>
2025-02-04 22:59:31 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Lamp Exchange</h1>
+        <p>Start trading now with confidence!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
